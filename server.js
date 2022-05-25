0c2edac3c95c10a29130e054f9aa70b1f0e114ae
rvallely <68432429+rvallely@users.noreply.github.com>
2022-05-25 16:03:32 +0100

    update cors header

--- server.js
+++ server.js
@@ -7,13 +7,17 @@ const { getBaseConversion } = require('./controllers/base-converter.controllers.
 const { handle404s, handleCustomErrors, handleServerErrors } = require('./errors/errors.js');
 
 app.use(cors());
-
-app.options('*', (req, res, next) => {
+app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, PUT,POST, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Authorization, Content-Length, X-Requested-With');
-    res.send(200);
-});
+    next();
+  });
+
+// app.options('*', (req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Authorization, Content-Length, X-Requested-With');
+//     res.send(200);
+// });
 
 app.use(express.json());
 
