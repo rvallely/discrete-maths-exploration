ad79d0e171686c1127a3d15f7550d90797bf8cd0
rvallely <rosievallely@hotmail.com>
2023-08-24 17:06:49 +0100

    add tests for checkListValid

---
+++ __tests__/checkListValid.test.js
@@ -0,0 +1,14 @@
+const { checkListValid } = require('../src/controllers/getSortedList');
+
+describe('#checkListValid', () => {
+    describe('given a string only containing numbers, commas and spaces', () => {
+        it('returns property listValid: true', () => {
+            expect(checkListValid('2, 10, 7, 8').listValid).toBe(true);
+        });
+    });
+    describe('given a string not only containing numbers, commas and spaces', () => {
+        it('returns property listValid: true', () => {
+            expect(checkListValid('2, 10, s7, 8.').listValid).toBe(false);
+        });
+    });
+});
