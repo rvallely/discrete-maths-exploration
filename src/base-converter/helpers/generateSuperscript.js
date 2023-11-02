2b7b98d8c64ef5165421d9bdcc662dd59c7df342
rvallely <rosievallely@hotmail.com>
2023-11-02 14:52:42 +0000

    remove unnessary script param

--- src/base-converter/helpers/generateSuperscript.js
+++ src/base-converter/helpers/generateSuperscript.js
@@ -1,3 +1,4 @@
+/* eslint-disable quote-props */
 exports.generateSuperscript = (number) => {
     const digitToSuperscriptMapper = {
         '0': '⁰',
@@ -9,7 +10,7 @@ exports.generateSuperscript = (number) => {
         '6': '⁶',
         '7': '⁷',
         '8': '⁸',
-        '9': '⁹'
+        '9': '⁹',
     };
     return number.split('').map((char) => digitToSuperscriptMapper[char]).join('');
 };
